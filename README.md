5b40064baf22ce729ab86b44b2749cd509145271
Bill Jones <misterunix@gmail.com>
2022-02-12 00:35:42 -0600

    much faster

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ Single loop - failed - took 15 seconds but it had the wrong results
 
 
 Double loops 600000 Time 2h21m6.07087493s
+
+Double loops with rectangle bounding
+6000 Time 16.838954968s **WOW**
+60000 Time 3m36.471748631s
+600000
--- main.go
+++ main.go
@@ -11,6 +11,13 @@ import (
 	"time"
 )
 
+type Rec struct {
+	MinX int
+	MinY int
+	MaxX int
+	MaxY int
+}
+
 var maxwidth int
 var maxheight int
 var maxsize int
@@ -19,14 +26,16 @@ var centerheight int
 
 var grid1 []int
 
+var wrec Rec
+
 func topple() bool {
 	var bail bool
 	var grid2 = make([]int, maxsize)
 
 	bail = true
 
-	for y := 0; y < maxheight; y++ {
-		for x := 0; x < maxwidth; x++ {
+	for y := wrec.MinY; y <= wrec.MaxY; y++ {
+		for x := wrec.MinX; x <= wrec.MaxX; x++ {
 			index := y*maxwidth + x
 			if grid1[index] < 4 {
 				grid2[index] = grid1[index]
@@ -34,8 +43,11 @@ func topple() bool {
 		}
 	}
 
-	for y := 0; y < maxheight; y++ {
-		for x := 0; x < maxwidth; x++ {
+	w := wrec
+	//for y := 0; y < maxheight; y++ {
+	//	for x := 0; x < maxwidth; x++ {
+	for y := w.MinY; y <= w.MaxY; y++ {
+		for x := w.MinX; x <= w.MaxX; x++ {
 
 			index := y*maxwidth + x
 			num := grid1[index]
@@ -48,6 +60,9 @@ func topple() bool {
 				if x-1 >= 0 {
 					tx := x - 1
 					grid2[y*maxwidth+tx]++
+					if tx < wrec.MinX {
+						wrec.MinX = tx
+					}
 					if grid2[y*maxwidth+tx] >= 4 {
 						bail = false
 					}
@@ -55,6 +70,9 @@ func topple() bool {
 				if y-1 >= 0 {
 					ty := y - 1
 					grid2[ty*maxwidth+x]++
+					if ty < wrec.MinY {
+						wrec.MinY = ty
+					}
 					if grid2[ty*maxwidth+x] >= 4 {
 						bail = false
 					}
@@ -63,6 +81,9 @@ func topple() bool {
 				if x+1 <= maxwidth-1 {
 					tx := x + 1
 					grid2[y*maxwidth+tx]++
+					if tx > wrec.MaxX {
+						wrec.MaxX = tx
+					}
 					if grid2[y*maxwidth+tx] >= 4 {
 						bail = false
 					}
@@ -70,6 +91,9 @@ func topple() bool {
 				if y+1 <= maxheight-1 {
 					ty := y + 1
 					grid2[ty*maxwidth+x]++
+					if ty > wrec.MaxY {
+						wrec.MaxY = ty
+					}
 					if grid2[ty*maxwidth+x] >= 4 {
 						bail = false
 					}
@@ -117,12 +141,43 @@ func main() {
 	centerheight = maxheight / 2
 	maxsize = maxheight * maxwidth
 
+	wrec.MaxX = 0
+	wrec.MaxY = 0
+	wrec.MinX = maxwidth
+	wrec.MinY = maxheight
+
 	grid1 = make([]int, maxsize)
 	//	grid2 = make([]int, maxsize)
 
 	index := centerheight*maxwidth + centerwidth
 	grid1[index] = 600000
 	//printboard()
+
+	// Scan grid to find working rectangle
+	for y := 0; y < maxheight; y++ {
+		for x := 0; x < maxwidth; x++ {
+			index := y*maxwidth + x
+			if grid1[index] != 0 && x < wrec.MinX {
+				wrec.MinX = x
+			}
+			if grid1[index] != 0 && y < wrec.MinY {
+				wrec.MinY = y
+			}
+			if grid1[index] != 0 && x > wrec.MaxX {
+				wrec.MaxX = x
+			}
+			if grid1[index] != 0 && y > wrec.MaxY {
+				wrec.MaxY = y
+			}
+		}
+	}
+
+	wrec.MinX--
+	wrec.MinY--
+	wrec.MaxX++
+	wrec.MaxY++
+
+	fmt.Printf("Min X:Y %d:%d Max X:Y %d:%d\n", wrec.MinX, wrec.MinY, wrec.MaxX, wrec.MaxY)
 
 	frame := 0
 	ty := 0
